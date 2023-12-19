d5cf62fe940e81b242328ee60e10c27a62a463f8
Tanishq Jain <tanishqjain2114@gmail.com>
2023-12-19 21:29:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductsCard from "./components/ProductsCard/index.jsx";
+
+const App = () => {
+    return <div className="productCardWrapper">
+        <ProductsCard/>
+    </div>;
+};
+
+export default App;
